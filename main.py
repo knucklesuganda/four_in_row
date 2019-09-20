d60dcda779a0f70da60cd947692d4c16033a8010
Silver <silveron.silveron@mail.ru>
2019-09-20 08:22:38 -0700

    asd

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ class Main:
         self.pos_x = 3
         self.field = np.zeros((5, 5))
 
+        self.current_player = 1
+        self.max_player = 2
+
     def main(self):
         show = Thread(target=self.__show_field)
         listener = Listener(on_release=self.move)
@@ -46,13 +49,22 @@ class Main:
                 print("Position is occupied!\n", exc)
                 sleep(2)
 
+            self.check_win()
+            self.switch_player()
+
     def drop_cell(self):
         current_y = len(self.field[self.pos_x]) - 1
 
         while self.field[current_y][self.pos_x] != 0:
             current_y -= 1
 
-        self.field[current_y][self.pos_x] = 1
+        self.field[current_y][self.pos_x] = self.current_player
+
+    def switch_player(self):
+        if self.current_player < self.max_player:
+            self.current_player += 1
+        else:
+            self.current_player = 1
 
     def __show_field(self):
         while True:
@@ -61,6 +73,18 @@ class Main:
 
             sleep(0.1)
             system("cls")
+
+    def check_win(self):
+        one_field = self.field.copy()
+
+        diagonal_left = one_field.diagonal()
+        diagonal_right = np.diag(np.rot90(one_field))
+        sum_number = self.current_player * 4
+
+        if diagonal_left.prod() == sum_number or diagonal_right.prod() == sum_number:
+            print("You won!")
+            sleep(10)
+            exit(0)
 
 
 if __name__ == '__main__':
